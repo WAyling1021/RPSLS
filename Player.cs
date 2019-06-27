839d9f347e28730c6f4174ddd502bfc2e08b0650
WAyling1021 <william.ayling15@gmail.com>
2019-06-27 07:22:29 -0500

    finished my display rules class, when you run the display rules class it displays the rules one by one as you hit enter.

---
+++ Player.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPSLS
+{
+    class MakeChoice
+    {
+        //member variables 
+        string choice;
+        int chances; 
+
+        //constructor 
+        public MakeChoice(string Choice)
+        {
+            this.choice = Choice;
+            int = 3;
+        }
+
+        //member methods 
+        public MakeChoice()
+        {
+            Console.WriteLine("Please choose rock, paper, scssior, lizard, spock");
+            choice = Console.ReadLine(); 
+        }
+        public MakeChoice()
+        {
+
+        }
+        public MakeChoice()
+        {
+
+        }
+    }
+}
---
+++ Rules.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RPSLS
+{
+    class Rules
+    {
+        //member variables 
+
+        //constructor 
+
+        //member methods 
+        List<string> RulesOfTheGame = new List<string>();
+
+        public void DisplayRules() 
+        {
+            RulesOfTheGame.Add("Scissors cuts paper");
+            RulesOfTheGame.Add("paper covers rock");
+            RulesOfTheGame.Add("rock crushes lizard");
+            RulesOfTheGame.Add("lizard poisons Spock");
+            RulesOfTheGame.Add("Spock smashes scissors");
+            RulesOfTheGame.Add("scissors decapitates lizard");
+            RulesOfTheGame.Add("lizard eats paper");
+            RulesOfTheGame.Add("paper disproves Spock");
+            RulesOfTheGame.Add("Spock vaporizes rock, and as it always has");
+            RulesOfTheGame.Add("rock crushes scissors");
+
+
+
+            foreach (string rule in RulesOfTheGame)
+            {
+                Console.WriteLine(rule);
+                Console.ReadLine();
+            }
+        }
+    }
+    
+    
+    
+    
+   
+}
